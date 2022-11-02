b9c87c36e2857bf03a2b4dbdfd8da70667d76ce5
Alexandre Bernard <alexandre.bernard@ekee.io>
2022-11-02 17:14:23 +0100

    GitHub workflow to handle deployments of echo server (#1)
    * add echo-server and the Dockerfile to build it
    
    Signed-off-by: Dot-H <alexandre.bernard@epita.fr>
    
    * github workflows
    
    Signed-off-by: Dot-H <alexandre.bernard@epita.fr>
    
    Signed-off-by: Dot-H <alexandre.bernard@epita.fr>

---
+++ echo-server/README.md
@@ -0,0 +1,4 @@
+# echo-server
+
+echo-server exposes a server listening on :8080 and echoing
+everything it receives
---
+++ echo-server/echo-server.go
@@ -0,0 +1,28 @@
+// echo-server exposes a server listening on :8080 and echoing
+// everything it receives
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+type EchoHandler struct{}
+
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
+	_, bodyE := io.Copy(w, rq.Body)
+	if bodyE != nil {
+		_, writeE := w.Write([]byte("failed to copy the body"))
+		if writeE != nil {
+			log.Printf("failed to write error to response body: %s", writeE)
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.Handle("/", &EchoHandler{})
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
